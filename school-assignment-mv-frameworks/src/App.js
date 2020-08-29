11ac8c1a3398d3d8941f2e5a65a691475633fc8e
patriciadunphy <dunphy.pm@gmail.com>
2020-08-29 18:42:05 +0200

    Added addToList component

--- school-assignment-mv-frameworks/src/App.js
+++ school-assignment-mv-frameworks/src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import AddToList from './AddToList';
+import ListItem from './components/ListItem';
+import List from './components/List';
+import axios from 'axios';
+import results from './results';
 
-function App() {
+
+
+class App extends Component {
+  state = {
+    listItems: []
+  }
+
+  addToList = (title, year) => {
+    results.post('/movies.json', {
+      title: title,
+      year: year,
+      completed: false
+    })
+    .then(res => this.setState({listItems: 
+      [...this.state.listItems, res.data]
+  })); 
   
-  return (
-    <div className="App">
-      <AddToList />
-    </div>
-  );
+  
+  }
+  render(){
+    return (
+      <div className="App">
+        <AddToList addToList={this.addToList}/>
+      </div>
+    );
+  }   
 }
 
 export default App;
