9c2242e587b5af64c7ca98037363fb81a795759a
patriciadunphy <dunphy.pm@gmail.com>
2020-08-31 11:10:28 +0200

    Add to list function

--- school-assignment-mv-frameworks/src/AddToList.js
+++ school-assignment-mv-frameworks/src/AddToList.js
@@ -1,42 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import results from './results';
 
-class AddToList extends React.Component{
+class AddToList extends Component{
     state = {
         title: '',
         year: ''
     }
-
-    onSubmit = (e) =>{
+    onSubmit = (e) => {
         e.preventDefault();
         const Data = {
             title: this.state.title,
-            year: this.state.year
+            year: this.state.year,
         }
-
-        // results.post('/movies.json', Data).then(response=>{
-        //     console.log(response);
-        // })
+        this.props.addToList(Data.title, Data.year);
     }
+     
+    onChange = (e) => this.setState({[e.target.name]:e.target.value});
 
-    render(){
+    render() {
         return (
-            <div>
-                <form className="movieForm" onSubmit={this.onSubmit}>
-                    <input type="text" placeholder="Enter title" value={this.state.title} 
-                    onChange={(e)=>this.setState({title: e.target.value})}/>
-                    <input type="text" placeholder="Enter year (optional)" value={this.state.year}
-                    onChange={(e)=>this.setState({year: e.target.value})}/>
-                    <button className="submitButton">Submit</button>
-                </form> 
-            </div>
+            <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
+                <input 
+                type="text" 
+                name="title" 
+                placeholder="Add Movie ..."
+                value={this.state.title}
+                onChange={this.onChange}
+                />
+                <input 
+                type="text" 
+                name="year" 
+                placeholder="Add Year ..."
+                value={this.state.year}
+                onChange={this.onChange}
+                />
+                <input 
+                type="submit" 
+                value="Submit" 
+                className="submitButton" 
+                />
+            </form>
         )
     }
 }
 
-// AddToList.propTypes = {
-//     addToList: PropTypes.func.isRequired
-// }
+AddToList.propTypes = {
+    addToList: PropTypes.func.isRequired
+}
 
 export default AddToList;
