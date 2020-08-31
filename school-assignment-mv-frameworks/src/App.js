e3170afe43bbd4d7c58c9c4c0ee68e40dcfe5681
patriciadunphy <dunphy.pm@gmail.com>
2020-08-31 12:02:42 +0200

    List and functionality for listItems

--- school-assignment-mv-frameworks/src/App.js
+++ school-assignment-mv-frameworks/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import AddToList from './AddToList';
+import AddToList from './components/AddToList';
 import ListItem from './components/ListItem';
 import List from './components/List';
 import axios from 'axios';
 import results from './results';
+import Header from './components/layout/Header';
+import About from './components/pages/About';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 
 
@@ -18,6 +21,21 @@ class App extends Component {
     .then(res => this.setState({ listItems: res.data}))
   }
 
+  //Complete item
+  markComplete = (id) => {
+    this.setState({listItems: this.state.listItems.map(listItem=>{
+      if(listItem.id === id){
+        listItem.completed = !listItem.completed
+      }
+      return listItem;
+    })});
+  }
+  //Delete item
+  delete = (id) =>{
+  results.delete(`/movies${id}`)
+    .then(res => this.setState({listItems: [...this.state.listItems.filter(listItem=>listItem.id!==id)]}));
+}
+  //Add item to list
   addToList = (title, year) => {
     results.post('/movies.json', {
       title: title,
@@ -27,17 +45,30 @@ class App extends Component {
     .then(res => this.setState({listItems: 
       [...this.state.listItems, res.data]
   }))
-  ; 
+   
   
   
   }
   render(){
     return (
-      <div className="App">
-        <AddToList addToList={this.addToList}/>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Route exact path="/" render={props => (
+              <React.Fragment>
+                <AddToList addToList={this.addToList}/>
+                <List list={this.state.listItems} markComplete={this.markComplete}
+                delete={this.delete}/>
+              </React.Fragment>
+            )} />
+            <Route path="/about" component={About} />          
+          </div>
+        </div>
+      </Router>
     );
   }   
 }
 
 export default App;
+
